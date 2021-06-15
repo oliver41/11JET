896bac214b13cb4f948830acaa4860b6d6e99c0d
Oliver Pfeiffer <oliverpfeiffer@me.com>
2021-06-15 11:35:50 +0200

    back to original

--- .eleventy.js
+++ .eleventy.js
@@ -4,7 +4,7 @@ module.exports = function (eleventyConfig) {
   // Layout aliases for convenience
   eleventyConfig.addLayoutAlias("default", "layouts/base.njk");
 
-  eleventyConfig.addPassthroughCopy("./src/css/styles.min.css");
+  eleventyConfig.addPassthroughCopy("./src/css/styles.css");
   eleventyConfig.addPassthroughCopy("./src/img");
   eleventyConfig.addPassthroughCopy({
     "./node_modules/alpinejs/dist/cdn.min.js": "js/alpine.js",
