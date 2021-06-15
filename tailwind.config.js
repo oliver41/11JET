3551d8065c6a1e0b67806ac7367a22c8951908d2
Oliver Pfeiffer <oliverpfeiffer@me.com>
2021-06-15 11:19:49 +0200

    adjusted package.json

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,7 @@
 const colors = require("tailwindcss/colors");
 
 module.exports = {
-  purge: ["./src/**/*.html"],
+  purge: ["./src/**/*.html", "./src/**/*.njk"],
   darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {
