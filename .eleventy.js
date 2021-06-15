5e8854695e1e963b7fe438607c9fd06fcf34aaff
Oliver Pfeiffer <oliverpfeiffer@me.com>
2021-06-15 10:18:52 +0200

    Initial commit

---
+++ .eleventy.js
@@ -0,0 +1,19 @@
+const htmlmin = require("html-minifier");
+
+module.exports = function (eleventyConfig) {
+  // Layout aliases for convenience
+  eleventyConfig.addLayoutAlias("default", "layouts/base.njk");
+
+  eleventyConfig.addPassthroughCopy("./src/css/styles.css");
+  eleventyConfig.addPassthroughCopy("./src/img");
+  eleventyConfig.addPassthroughCopy({
+    "./node_modules/alpinejs/dist/cdn.min.js": "js/alpine.js",
+  });
+
+  return {
+    dir: {
+      input: "src",
+      output: "public",
+    },
+  };
+};
---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+const colors = require("tailwindcss/colors");
+
+module.exports = {
+  purge: ["./src/**/*.html"],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {
+      colors: {
+        "light-blue": colors.lightBlue,
+        cyan: colors.cyan,
+      },
+    },
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [require("@tailwindcss/typography")],
+};
